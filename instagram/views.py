b9c7875d150a5de2a979874916f77c18041eeaa6
ByeonghwiJeong <byeonghwijeong@gmail.com>
2022-08-24 10:34:28 +0900

    feat : 빌트인 CBV DetailView를 통한 post_detail 뷰 구현
    - django.shortcuts.get_objects_or_404 사용법
    > post = Post.objects.get(pk=pk)
    > > DoseNotExist인경우 예외처리 raise: django.http.Http404
    - django.views.generic.DetailVeiw .as_view(model=Post)

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse, HttpRequest, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # --- CBV ---
@@ -13,21 +13,27 @@ post_list = ListView.as_view(model = Post)
 #     q = request.GET.get('q', '')
 #     if q:
 #         qs = qs.filter(message__icontains=q)
-#     # instagram/templates/instagram/post_list << 위치고정
-#     return render(request, 'instagram/post_list.html', {
-#         'post_list': qs,
-#         'q': q
-#     })
+    # # instagram/templates/instagram/post_list << 위치고정
+    # return render(request, 'instagram/post_list.html', {
+    #     'post_list': qs,
+    #     'q': q
+    # })
     
 
-# 타입힌트
-def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    response = HttpResponse()
-    response.write("Hello World!")
-    response.write("Hello World!")
-    response.write("Hello World!")
-    return response
+# post = get_object_or_404(Post, pk=pk)
+# 아래와 같은 뜻
+    # try:
+    #     post = Post.objects.get(pk=pk) # DoseNotExist 예외
+    # except Post.DoesNotExist:
+    #     raise Http404 
+# 타입힌트  >>  (request: HttpRequest, pk: int) -> HttpResponse
+# def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'instagram/post_detail.html',{
+#         'post': post,
+#     })
 
+post_detail = DetailView.as_view(model=Post)
 
 def archives_year(request, year):
     return HttpResponse(f"{year}년 archives")
