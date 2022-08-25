b710f25376843390dcdcede89a3e4c6e8cb6efb1
ByeonghwiJeong <byeonghwijeong@gmail.com>
2022-08-26 03:28:04 +0900

    feat	: PostForm에 clean_message 커스텀 유효성 검사 추가

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from .models import Post
 
@@ -8,3 +10,9 @@ class PostForm(forms.ModelForm):
         fields = ['message', 'photo', 'tag_set', 'is_public'] 
         # Author 보여지지 않도록함, is_valid() 에서 유효성검사도 제외
         # exclude = []
+
+    def clean_message(self):
+        message = self.cleaned_data.get('message')
+        if message:
+            message = re.sub(r'[a-zA-Z]+', ' ', message)
+        return message
